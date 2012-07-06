6968f7f173a98ca58111b37ad3893c77b4f45b66
Krishna Karthik <cris@karthik-k.(none)>
2012-07-06 13:28:34 +0530

    Switched to recent selenium version to work with recent firefox

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject nolan "1.0.0-SNAPSHOT"
-  :description "Play the script and record the video"
+(defproject nolan "0.0.1"
+  :description "Play the selenium script and record the video"
+
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [javax.media/jmf "2.1.1e"]
-                 [org.seleniumhq.selenium/selenium-java "2.9.0"]]
+                 [org.seleniumhq.selenium/selenium-java "2.24.1"]]
+  
   :source-path "src/clj"
   :java-source-path "src/java"
 
@@ -12,4 +14,5 @@
   ;; un-comment to enable remote debugging
   ;; :jvm-opts ["-Xdebug"
   ;;            "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5004"]
-  )
+
+  :plugins [[lein-swank "1.4.3"]])
